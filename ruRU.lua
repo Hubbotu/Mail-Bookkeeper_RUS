2ef73660096b3563422d9a098476d7de40acf124
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-06-16 07:25:52 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,14 @@
+local folder, core = ...
+local L = LibStub("AceLocale-3.0"):NewLocale(folder, "ruRU", true)
+
+L.PoI = "Люди, представляющие интерес"
+L.PoIDesc = "Имена покупателей, которых вы хотите выделить."
+L.fwDesc = "Скрыть записи за пределами этого времени"
+L.receviedMail = "Ты сделал |cffffffff%d|r общий объем продаж %s."
+L["Показать продажи, сохраненные с помощью параметра 'сохранить продажи'."] = true
+L.showSalesChange = "Показать продажи, автоматически настроенные на |cffffffff%s|r."
+--~ L["Source"] = true
+--~ L["Auction"] = true
+--~ L["COD"] = true
+--~ L[""] = true
+--~ L[""] = true
